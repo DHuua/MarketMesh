dc2192a91be5a6063c977343c6784fe4286a6fc2
Mukamet Muratbek <mukametmuratbek@gmail.com>
2025-10-28 23:08:00 +0500

    added RestAPI and it correctly working

--- src/main/java/com/hero/marketmesh/MarketMeshApplication.java
+++ src/main/java/com/hero/marketmesh/MarketMeshApplication.java
@@ -1,13 +1,11 @@
-package Hero.MarketMesh;
+package com.hero.marketmesh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MarketMeshApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MarketMeshApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MarketMeshApplication.class, args);
+    }
 }
