e965a9d670cace905e54411d59a51c6a7b6d2637
Mukamet Muratbek <mukametmuratbek@gmail.com>
2025-10-28 21:24:47 +0500

    Initial commit

---
+++ src/main/java/Hero/MarketMesh/MarketMeshApplication.java
@@ -0,0 +1,13 @@
+package Hero.MarketMesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketMeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketMeshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Hero/MarketMesh/MarketMeshApplicationTests.java
@@ -0,0 +1,13 @@
+package Hero.MarketMesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketMeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
